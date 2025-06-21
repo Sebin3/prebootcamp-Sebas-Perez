c5dc69a74c5fd9cd68d329bf05a63353afd01cd0
Sebas Perez <isvxlez@gmail.com>
2025-06-21 00:03:09 -0500

    Delete Intermedio directory

--- Intermedio/app.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("name");
-const button = document.getElementById("button");
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  const nombre = input.value;
-  Swal.fire('Hola, ' + nombre + '!');
-});
